3bfcca21fbc085d1a937c93295489b2f7ccc86f4
Sam Maxwell <maxwell.g.sam@gmail.com>
2017-04-07 22:20:07 +0100

    -GUI Classes removed, the code should initially work in the command line before any GUI functionality is added. The GUI should almost act as a wrapper for the console function of the code. -Code error reporting improved, any core functions such as file reads or serial communications now has a boolean that tracks whether or not the function executed successfully. This should allow for fast error handling and the ability to decipher where the error occured. -Commenting improved in the code. -Wait time added to the Send Line function to allow the printer time to respond. -Close file function added.

--- Printer Host.py
+++ Printer Host.py
@@ -7,23 +7,22 @@ Created on Sat Feb 25 15:01:23 2017
 This is the main software file for the Maxwell Printer Host Application. The Maxwell Printer Host is a piece of software designed to
 communicate with 3D Printers that run open source control firmware.
 """
-
 #--------------------------------------------------------------------------------------------------------------------------------------------------------------------
 #MODULES
 #--------------------------------------------------------------------------------------------------------------------------------------------------------------------
 import serial
 import sys
-from PyQt4 import QtGui
-#--------------------------------------------------------------------------------------------------------------------------------------------------------------------
-#DEFINITIONS
-#--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+from PyQt4 import QtCore, QtGui
+import time
 #--------------------------------------------------------------------------------------------------------------------------------------------------------------------
 #CLASSES
 #--------------------------------------------------------------------------------------------------------------------------------------------------------------------
 #A Class Designed to complete all base communications with the 3D Printer and to store base printer information
 class printer():
     #Definitions
-    
+    #Serial Communication
+    serial_wait_time = 0.1          #The amount of time to wait before attempting to read the printers response
+
     #Tracking Variables
     x = False       #A Variable to track the X Position of the Printer
     y = False       #A Variable to track the Y Position of the Printer
@@ -31,99 +30,147 @@ class printer():
     
     #Constructor, generates a new object of the Printer Class with default values
     def __init__(self, com_port, baud_rate):
-        #Establishing Serial Communication with the device
-        self.port = serial.Serial(port = com_port, baudrate = baud_rate)
+        #Creating a Boolean to Track whether the code executed successfully or not
+        code_successful = True
+        
+        #Attempting to establish Serial Communication with the Printer
+        try:
+            #Establishing Serial Communication with the device
+            self.port = serial.Serial(port = com_port, baudrate = baud_rate)
+            
+            #Flushing the Input Buffer to remove any remaining information that may have been previously recieved
+            self.port.flushInput()
+            
+            #Flushing the Output Buffer to remove any remaining information that may have previously failed to send
+            self.port.flushOutput()
+        
+        #Serial access Error
+        except serial.SerialException:
+            #Printing an Error Message to the Console
+            print "Failed to connect to the Printer, Serial Communications could not be initialised."
+            
+            #Setting Code successful to False to indicate that a Serial Error Occured
+            code_successful = False
+        
+        #Returning the Code Successful Boolean to indicate whether the function executed correctly or not
+        return code_successful
         
     #Method used to send a line of GCode to the Printer
     def send_line(self, gcode):
+        #Creating a Boolean to Track whether the code executed successfully or not
+        code_successful = True
+        
+        #Attempting to Send a Gcode Line to the Printer
         try:
-            #Sending the Line over the Serial Port
+            #Flushing the Serial Output Buffer to ensure that no residual data is sent with the Gcode line
+            self.port.flushOutput()
+            
+            #Sending the GCode Line Over the Serial Port to initialise a Printer Action
             self.port.write(gcode)
             
             #Generating a Variable to Temporarily store the Printers Reply
             printer_reply = ""
             
-            #Waiting for the Printers Reply
+            #Flushing the Serial Input Buffer to ensure that no residual data is read with the Printers Reply
+            self.port.flushInput()
+            
+            #Pausing to allow the Printer to Respond
+            time.sleep(self.serial_wait_time)
+            
+            #While there is information remaining in the Serial Input Buffer
             while self.port.inWaiting() > 0:
-                #Reading the reply from the Printer
-                byte = chr(self.port.read(1))
+                #Reading the next byte of information from the Serial Port
+                byte = self.port.read(1)
                 
-                #Adding the new byte to the Printer Reply String
+                #Appending the byte to the variable storing the Printers Reply String, this builds the reply string byte by byte
                 printer_reply += byte
             
-            #If the printer did not give a reply, return an error
-            if len(printer_reply) == 0:
-                #Setting the Value of Printer Reply to the Empty Reply Error
-                printer_reply = "No Reply"
+            #Getting the Length of the Printers Reply
+            length_printer_reply = len(printer_reply)
             
-            #Returning the Printers Reply
-            return printer_reply
-        #If a Serial Error Occurs, return an error
+            #If there was no response from the Printer
+            if length_printer_reply == 0:
+                #Setting the Code Successful Boolean to False to indicate that the GCode line failed to send
+                code_successful = False
+            
+        #Serial access Error
         except serial.SerialException:
-            #Printing the Serial Error
-            return "Serial Error"
+            #Printing an Error Message to the Console
+            print "Failed to send GCode line due to Serial Communications error."
+            
+            #Setting the code successful Boolean to indicate that a Serial Error occured
+            code_successful = False
+        
+        #Returning the Printers Reply and the Code Successful Variable to indicate whether the function executed correctly or not
+        return printer_reply, code_successful
 
 #A Class Designed to complete all GCode File Interactions
 class gcode_file():
-    #Definitions
-    
-    #Tracking Variables
-    
-    
-    #Constructor, generates a new object of the GCode File Class
-    def __init__(self):
-        #Passing the Constructor
-        pass
-    
     #Method used to open the GCode File at the given Filepath
     def open_file(self, gcode_filepath, access_type):
-        #Opening the File with the given file path and access type
-        new_file = open(gcode_filepath, access_type)
+        #Creating a Boolean to track whether the function executes correctly or not
+        code_successful = True
         
-        #Updating the Classes current file with the new file
-        self.current_file = new_file
+        #Attempting to Open the GCode File
+        try:
+            #Opening the File with the given file path and access type
+            new_file = open(gcode_filepath, access_type)
+            
+            #Updating the Classes current file with the new file
+            self.current_file = new_file
+        
+        #File Access Error
+        except IOError:
+            #Printing an Error Message to the Console
+            print "Failed to open GCode file."
+            
+            #Setting the code successful variable to False to indicate that a File access Error Occured
+            code_successful = False
+        
+        #Returning the code successful boolean to indicate whether not the function executed successfully
+        return code_successful
+            
+    #Method used to close the currently opened file
+    def close_file(self):
+        #Creating a Boolean to track whether the function executes correctly or not
+        code_successful = True
+        
+        #Attempting to Close the GCode File
+        try:
+            #Closing the current file
+            self.current_file.close()
+            
+        #File access Error
+        except IOError:
+            #Printing an Error Message to the Console
+            print "Failed to close GCode file."
+        
+            #Setting the code successful boolean to False to indicate that a File access error ocurred
+            code_successful = False
+        
+        #Returning the code successful boolean to inidcate whether or not the function executed successfully
+        return code_successful
         
     #Method used to read a given number of lines from the GCode File
     def read_gcode_lines(self, num_lines):
-        #Reading the given number of lines from the Gcode File
-        gcode_lines = self.current_file.readlines(num_lines)
+        #Creating a Boolean to track whether the function executes correctly or not
+        code_successful = True
         
-        #Returning the Lines read from the GCode File
-        return gcode_lines
-
-#GUI Classes
-#Application Class
-class main_host_application(QtGui.QApplication):
-    #Constructor, generates a new Maxwell Printer Host Application GUI
-    def __init__(self):
-        #Inheriting from the QApplication Class
-        super(main_host_application, self).__init__()
+        #Attempting to Read the given number of lines from the Gcode File
+        try:
+            #Reading the given number of lines from the Gcode File
+            gcode_lines = self.current_file.readlines(num_lines)
         
-        #Running the Main Event Handler Loop of the Application
-        sys.exit(self.exec_())
-    
-#Main Window Class
-class main_host_window(QtGui.QWidget):
-    #Definitions
-    window_title = "Maxwell Printer Host"
-
-    #Tracking Variables
-    
-    #Constructor, generates a new Maxwell Printer Host Main Window
-    def __init__(self):
-        #Inheriting from the QWidget Class
-        super(main_host_window, self).__init__()
+        #File access Error
+        except IOError:
+            #Printing an Error Message to the Console
+            print "Failed to read lines from the GCode file."
+            
+            #Setting the code successful boolean to False to indicate that a File access error ocurred
+            code_successful = False
         
-        #Setting the Title of the Main Window
-        self.setWindowTitle(self.window_title)
-        
-        #Setting the Geometry of the Main Window
-        self.setGeometry(1000, 600)
-        
-        #Creating a Grid Layout for the Main Window
-        glay = QtGui.QGridLayout()
-        
-        #
+        #Returning the Lines read from the GCode File and the code successful boolean to indicate whether or not the function executed successfully
+        return gcode_lines, code_successful
 #--------------------------------------------------------------------------------------------------------------------------------------------------------------------
 #MAIN CODE
 #--------------------------------------------------------------------------------------------------------------------------------------------------------------------
